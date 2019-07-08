ad6c019f3d8390fa31e2237abf25e1aec41f4b66
Vegar Dietz <post@digitalkopi.no>
2019-07-08 14:16:11 +0200

    lesson-04

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="home">
         <h1>Homepage</h1>
+        <p class="red white--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            temporibus similique aut reiciendis at impedit repellat repellendus
+            rerum, amet totam eaque blanditiis a delectus odio nostrum
+            necessitatibus, maiores perferendis. Obcaecati.
+        </p>
+        <p class="pink lighten-4 red--text text--darken-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            rerum soluta ipsum ea cupiditate! Quaerat sit amet natus placeat
+            sint ullam ipsam eius odit, omnis unde! Dolore cupiditate veritatis
+            magni.
+        </p>
+        <h1 class="display-4">Massiv Display</h1>
+        <h4 class="display-1">Smaller Display</h4>
+        <p class="headline font-weight-bold">This is a headline</p>
+        <p class="subheading font-weight-black">This is a subheading</p>
+        <p class="caption">This is a caption</p>
+
+
     </div>
 </template>
 
