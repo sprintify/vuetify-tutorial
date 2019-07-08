65b141af3fb3bc212b18f81e2ebdb2df134c3d2c
Vegar Dietz <post@digitalkopi.no>
2019-07-08 14:39:35 +0200

    lesson-05

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
     <div class="home">
         <h1>Homepage</h1>
-        <p class="red white--text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-            temporibus similique aut reiciendis at impedit repellat repellendus
-            rerum, amet totam eaque blanditiis a delectus odio nostrum
-            necessitatibus, maiores perferendis. Obcaecati.
-        </p>
-        <p class="pink lighten-4 red--text text--darken-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            rerum soluta ipsum ea cupiditate! Quaerat sit amet natus placeat
-            sint ullam ipsam eius odit, omnis unde! Dolore cupiditate veritatis
-            magni.
-        </p>
-        <h1 class="display-4">Massiv Display</h1>
-        <h4 class="display-1">Smaller Display</h4>
-        <p class="headline font-weight-bold">This is a headline</p>
-        <p class="subheading font-weight-black">This is a subheading</p>
-        <p class="caption">This is a caption</p>
+        <v-btn class="pink white--text">click me</v-btn>
+        <v-btn depressed color="pink">click me</v-btn>
+        <v-btn flat color="pink">click me</v-btn>
 
+        <v-btn depressed class="pink white--text">
+            <v-icon left>email</v-icon>
+            <span>Email me</span>
+        </v-btn>
 
+        <v-btn depressed small class="pink white--text">
+            <v-icon left small>email</v-icon>
+            <span>Email me</span>
+        </v-btn>
+
+        <v-btn depressed large class="pink white--text">
+            <span>Email me</span>
+             <v-icon right large>email</v-icon>
+        </v-btn>
+
+        <v-btn fab depressed small dark color="purple">
+            <v-icon>favorite</v-icon>
+        </v-btn>
     </div>
 </template>
 
