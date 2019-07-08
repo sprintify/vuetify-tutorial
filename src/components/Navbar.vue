e5f28cf401d2067e9460717ac3a24f9e77caaa6a
Vegar Dietz <post@digitalkopi.no>
2019-07-08 20:07:52 +0200

    lesson-10

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,7 +17,18 @@
         </v-toolbar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
-            <p class="error">test</p>
+            <v-list>
+                <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-tile-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                        <!-- eslint-disable-next-line -->
+                        <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -26,7 +37,12 @@
 export default {
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'folder', text: 'My Projects', route: '/projects' },
+                { icon: 'person', text: 'Team', route: '/team' }
+            ]
         }
     }
 }
