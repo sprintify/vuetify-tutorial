c125f92c6a59afd678289ced9b5211a165a59300
Vegar Dietz <post@digitalkopi.no>
2019-07-09 09:05:30 +0200

    lesson-17

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,18 @@
     <div class="dashboard">
         <h1 class="subheading grey--text">Dashboard</h1>
         <v-container class="my-5">
+            <v-layout row class="mb-3">
+                <!-- Listing by folders -->
+                <v-btn small flat color="grey" @click="sortBy('title')">
+                    <v-icon left small>folder</v-icon>
+                    <span class="caption text-lowercase">By project name</span>
+                </v-btn>
+                <!-- Listing by name -->
+                <v-btn small flat color="grey" @click="sortBy('person')">
+                    <v-icon left small>person</v-icon>
+                    <span class="caption text-lowercase">By person</span>
+                </v-btn>
+            </v-layout>
             <v-card flat v-for="project in projects" :key="project.title">
                 <v-layout row wrap :class="`pa-3 project ${project.status}`">
                     <v-flex xs12 md6>
@@ -74,6 +86,11 @@ export default {
                         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
                 }
             ]
+        }
+    },
+    methods: {
+        sortBy(prop) {
+            this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1))
         }
     }
 }
